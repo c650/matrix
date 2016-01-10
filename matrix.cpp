b183b4d80f00f4aa6b4bd071dce65eab33c28533
c650 <cbailey@palmertrinity.org>
2016-01-10 17:16:25 -0500

    re-documented

--- matrix.cpp
+++ matrix.cpp
@@ -1,8 +1,11 @@
 /*
-	A Matrix implementation to ease future problems that
+	matrix.cpp
+
+	Purpose: A Matrix implementation to ease future problems that
 	could more efficiently be solved with Matrix abstraction.
 
-	by: Charles Bailey
+	@author c650
+	@version 0.1 1/10/16
 */
 #include <vector>
 #include <iostream>
@@ -20,13 +23,35 @@ class Matrix {
 	vector< vector<T> > elements;
 
   public:
+/*
+	Constructors:
+*/
+	/*
+		Makes a new instance of Matrix
+
+		@param elem a 2D vector that represents what a
+		Matrix actually is.
+
+		@return a new instance of Matrix
+	*/
+  	Matrix (vector< vector<T> > elem) : elements(elem) {}
   	/*
-		Constructors
-  	*/
-  	Matrix (vector< vector<T> > e) : elements(e) {}
-  	
+		Makes a new instance of Matrix
+
+		@param m another instance of Matrix to copy
+		using func get_all_elements()
+
+		@return a new instance of Matrix
+	*/
   	Matrix (Matrix& m) : elements(m.get_all_elements()) {}
-  	
+  	/*
+		Makes a new, empty instance of Matrix
+
+		@param rows the number of rows desired
+		@param cols the number of columns desired
+
+		@return a new instance of Matrix
+	*/
   	Matrix (const int& rows, const int& cols) {
   		for (int i = 0; i < rows; i++) {
   			vector<T> tmp(cols);
@@ -34,33 +59,64 @@ class Matrix {
   		}
   	}
 
-  	/*
-		Column and Row Size Getters:
-  	*/
+/*
+	Column and Row Size Getters:
+*/
+	/*
+		Returns the number of columns in an instance
+		of Matrix
 
+		@return the number of columns
+	*/
 	int getcol() {
 		return elements[0].size();
 	}
+	/*
+		Returns the number of rows for an instance of
+		Matrix
 
+		@return the number of columns
+	*/
 	int getrow() {
 		return elements.size();
 	}
-	/*
-		Element Getter and Setter:
-	*/
 
+/*
+	Element Getter and Setter:
+*/
+	/*
+		Changes the value of a given position in
+		the Matrix.
+
+		@param row the row that holds the value to change
+		@param col the column of the value to change
+		@param val the value to set
+	*/
 	void set(const int& row, const int& col, T const& val) {
 		if (this->getcol() > col && this->getrow() > row)
 			elements[row][col] = val;
 	}
+	/*
+		Gets the value held at a certain position in
+		the Matrix.
 
+		@param row the row to read from
+		@param col the column to read from
+		
+		@return the value of type T held at the positon
+		row,col in the Matrix
+	*/
 	T get(const int& row, const int& col) {
 		if (this->getcol() > col && this->getrow() > row)
 			return elements[row][col];
 		else
 			return 0;
 	}
+	/*
+		Helper method for the second Matrix constructor.
 
+		@return a copy of a Matrix's `elements` attribute
+	*/
 	vector< vector<T> > get_all_elements() {
 		// make temp vector
 		vector< vector<T> > tmp;
@@ -69,9 +125,66 @@ class Matrix {
 
 		return tmp;
 	}
+/*
+	Matrix Arithmetic Methods
+*/
+	/*
+		ADDITION:
 
+		Binary operator to add two matrices together.
+		Does not modify either matrix.
+
+		@param other the Matrix on the right side of +
+
+		@return a Matrix, whose elements are the sums of
+		the corresponding elements of `this` and `other`
+	*/
+	Matrix<T> operator+(Matrix<T>& other) {
+		// Matrices must be same dimensions to
+		// be addable
+		if (this->getrow() != other.getrow() 
+			|| this->getcol() != other.getcol())
+			return *this;
+
+		// make matrix based on `this`
+		Matrix<T> resultant(*this);
+
+		for (int i = 0, n = this->getrow(); i < n; i++) {
+			for (int j = 0, o = this->getcol(); j < o; j++) {
+				// set each value in the resultant equal
+				// to the sum of the corresponding values
+				// in `this` and `other`
+				resultant.set(i,j,this->get(i,j) + other.get(i,j));
+			}
+		}
+
+		return resultant;
+	}
+	/*
+		SUBTRACTION:
+
+		Binary operator to subtract two matrices.
+		Does not modify either matrix.
+
+		@param other the Matrix on the right side of -
+
+		@return a Matrix, whose elements are the differences of
+		the corresponding elements of `this` and `other`
+	*/
+	Matrix<T> operator-(Matrix<T>& other) {
+		return (*this) + (other * -1);
+	}
   	/*
-		Implement Matrix Multiplication
+		MATRIX*MATRIX MULTIPLICATION
+
+		Binary operator to multiple two matrices.
+		Does not modify either matrix.
+
+		@param other the Matrix on the right side of *
+
+		@return a Matrix, whose elements are the products of
+		the corresponding elements of `this` and `other`, following
+		the accepted way of multiplying matrices.
   	*/
 	Matrix<T> operator*(Matrix<T>& other) {
 		Matrix<T> resultant(this->getrow(), other.getcol());
@@ -103,14 +216,38 @@ class Matrix {
 		return resultant;
 	}
 	/*
-		Matrix Power Operator
+		MATRIX*SCALAR MULTIPLICATION
 
-		NOTE: This needs to be optimized!!!
+		Multiplies a Matrix by a scalar value.
 
+		@param other the scalar value
+
+		@return the result of scaling `this` by `other`
+	*/
+	Matrix<T> operator*(T& other) {
+		Matrix<T> resultant(*this);
+
+		for (int i = 0, n = this->getrow(); i < n; i++) {
+			for (int j = 0, o = this->getcol(); j < o; j++) {
+				resultant.elements[i][j] *= other;
+			}
+		}
+
+		return resultant;
+	}
+	/*
+		Matrix Power Method
 		Powers >= 1 only!
+
+		@param p an unsigned integer >= 1 to which the Matrix will
+		be raised.
+
+		@return the Matrix raised to the `p` power
 	*/
 	Matrix power(const unsigned int& p) {
 		if (p == 1)
+			return *this;
+		else if (p <= 0) // set for now to keep it safe
 			return *this;
 
 		Matrix m(*this);
@@ -124,6 +261,7 @@ class Matrix {
 	}
 	/*
 		Matrix Printer
+		Formats the Matrix for STDOUT
 	*/
 	void print() {
 		for (int i = 0, n = this->getrow(); i < n; i++) {
@@ -135,3 +273,7 @@ class Matrix {
 		}
 	}
 };
+
+int main() {
+	return 0;
+}
